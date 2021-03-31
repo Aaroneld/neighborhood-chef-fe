f9cf6c90a9fe16600e784e34c9c49221bcc56a73
patrick-replogle <patrickr1138@gmail.com>
2021-03-31 15:23:53 -0700

    added a responsive event view card to the calendar when in tablet and mobile view

--- src/components/calender/calender-view/event-popup/EventPopup.styles.js
+++ src/components/calender/calender-view/event-popup/EventPopup.styles.js
@@ -1,19 +1,47 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const cardStyles = makeStyles((theme) => ({
-  card: {
+  root: {
     display: 'flex',
-    flexDirection: 'column',
-    width: '60%',
-    marginLeft: '25%',
-    fontSize: '1.2rem',
-    justifyContent: 'flex-start',
-    [theme.breakpoints.down('md')]: {
-      marginLeft: '20%',
+    width: '90%',
+    justifyContent: 'center',
+    marginLeft: '2%',
+
+    '& img': {
+      maxWidth: '30%',
+      maxHeight: '136px',
+      [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
     },
-    [theme.breakpoints.down('sm')]: {
-      width: '80%',
-      marginLeft: '10%',
+    '& .container': {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      width: '70%',
+      marginLeft: '1%',
+      fontSize: '1.2rem',
+      '& a, & p, & span': {
+        textAlign: 'left',
+      },
+      '& .buttons': {
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        textAlign: 'left',
+        width: '100%',
+        [theme.breakpoints.down('sm')]: {
+          textAlign: 'center',
+        },
+      },
+      [theme.breakpoints.down('sm')]: {
+        width: '80%',
+        marginLeft: '10%',
+        justifyContent: 'center',
+        '& a, & p, & span': {
+          textAlign: 'center',
+        },
+      },
     },
   },
 }));
