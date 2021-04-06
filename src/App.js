73e3685e90d4ef8930dd2054369b4739d72adf49
aaroneld <merrifield48@gmail.com>
2021-04-06 17:53:47 -0400

    turn off hidden overflow

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   }, []);
 
   return (
-    <div style={{ height: '100vh', width: '100vw', overflow: 'hidden' }}>
+    <div style={{ height: '100vh', width: '100vw' }}>
       <Switch>
         <Route path="/date">
           <DatePicker
