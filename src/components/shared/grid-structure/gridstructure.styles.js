8a42bfcc8736ecf6ecc690e4a5faf4f4b27e2af5
patrick-replogle <patrickr1138@gmail.com>
2021-04-21 14:09:58 -0700

    removed horizontal scrollbar

--- src/components/shared/grid-structure/gridstructure.styles.js
+++ src/components/shared/grid-structure/gridstructure.styles.js
@@ -4,45 +4,45 @@ export const styles = makeStyles((theme) => {
   return {
     'grid-container': {
       display: 'grid',
-      maxHeight: "100vh",
-      width: "100vw",
+      maxHeight: '100vh',
+      width: '100vw',
       'grid-template-columns': '2fr 8fr',
       'grid-template-rows': '1fr 5fr',
       gap: '1px 1px',
       'grid-template-areas': ' "Logo Header" "Sidebar Variable" ',
-      overflowY: "hidden",
+      overflowY: 'hidden',
+      overflowX: 'hidden',
 
       [theme.breakpoints.down('md')]: {
         display: 'flex',
         flexDirection: 'column',
-        minHeight: "100vh",
-        overflowY: "hidden"
+        minHeight: '100vh',
+        overflowY: 'hidden',
       },
     },
     Variable: {
-      width: "80vw",
+      width: '80vw',
       gridArea: 'Variable',
 
-      [theme.breakpoints.down("md")]: {
-        width: "100vw",
-        overflowY: "scroll"
-      }
+      [theme.breakpoints.down('md')]: {
+        width: '100vw',
+        overflowY: 'scroll',
+      },
     },
     Header: {
       gridArea: 'Header',
-      height: "16.66vh",
+      height: '16.66vh',
 
-      [theme.breakpoints.down("md")]: {
-        height: "10vh"
+      [theme.breakpoints.down('md')]: {
+        height: '10vh',
       },
-
     },
     Sidebar: {
       gridArea: 'Sidebar',
       paddingLeft: '5%',
 
       [theme.breakpoints.down('md')]: {
-        paddingLeft: 0
+        paddingLeft: 0,
       },
 
       [theme.breakpoints.down('sm')]: {
@@ -55,19 +55,19 @@ export const styles = makeStyles((theme) => {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'flex-start',
-      flexWrap: "nowrap",
-      height: "16.66vh",
-      maxWidth: "20vw",
+      flexWrap: 'nowrap',
+      height: '16.66vh',
+      maxWidth: '20vw',
 
       [theme.breakpoints.down('md')]: {
-        width: "100vw",
-        maxWidth: "100vw",
-        height: "7vh",
+        width: '100vw',
+        maxWidth: '100vw',
+        height: '7vh',
       },
 
-      [theme.breakpoints.down("xs")] : {
-        height: "10vh"
-      }
+      [theme.breakpoints.down('xs')]: {
+        height: '10vh',
+      },
     },
   };
 });
