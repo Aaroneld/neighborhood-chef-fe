ac3d6458a4cc6a315993f38640e3a3843ed67ce4
patrick-replogle <patrickr1138@gmail.com>
2021-05-06 12:18:07 -0700

    small profile style tweaks

--- src/components/profile/profile.styles.js
+++ src/components/profile/profile.styles.js
@@ -28,7 +28,6 @@ export const styles = makeStyles((theme) => ({
         height: '160px',
         fontSize: '5rem',
         border: '6px solid rgba(33, 186, 66, 0.75)',
-        padding: '5px',
       },
 
       '& h2': {
