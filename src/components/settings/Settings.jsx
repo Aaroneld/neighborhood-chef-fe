f04885e9e31b3dafa6504a9aa73a3ffd86673ffa
patrick-replogle <patrickr1138@gmail.com>
2021-05-09 11:23:34 -0700

    bare bones structure in place for settings page

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,38 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import * as yup from 'yup';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { styles } from './settings.styles';
+import useForm from '../../hooks/useForm.js';
+
+const validationSchema = yup.object().shape({
+  firstName: yup.string().required("'First name' is a required field"),
+  lastName: yup.string().required("'Last name' is a required field"),
+  biography: yup.string(),
+  address: yup
+    .string()
+    .required("'Address' is a required field - Selecting an option from the dropdown menu is required'"),
+  gender: yup.string(),
+  latitude: yup.number().required(),
+  longitude: yup.mixed().required(),
+});
 
 const Settings = () => {
+  const [charsLeft, setCharsLeft] = useState(255);
   const user = useSelector((state) => state.user);
+  const { values, setValues, validate, errors } = useForm(validationSchema);
   const classes = styles();
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  useEffect(() => {
+    if (Object.keys(user).length > 0) {
+      setValues({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        gender: user.gender,
+        address: user.address,
+        longitude: user.longitude,
+        latitude: user.latitude,
+        biography: user.biography,
+      });
+      console.log(user.biography.length);
+      setCharsLeft(255 - user.biography.length);
+    }
+  }, [user]);
+
   return (
     <Card className={classes.root}>
-      <div className="main">
-        <div className="textIconContainer">
+      <form className="main" onSubmit={handleSubmit}>
+        <div className="textInputContainer">
           <Typography variant="h6">First Name</Typography>
-          <Typography>{user.firstName}</Typography>
+          <input type="text" name="firstName" value={values.firstName} onChange={handleChange} />
         </div>
-        <div className="textIconContainer">
+        <div className="textInputContainer">
           <Typography variant="h6">Last Name</Typography>
-          <Typography>{user.lastName}</Typography>
+          <input type="text" name="lastName" value={values.lastName} onChange={handleChange} />
         </div>
-        <div className="textIconContainer">
+        <div className="textInputContainer">
           <Typography variant="h6">Address</Typography>
           <Typography>{user.address}</Typography>
         </div>
-        <div className="textIconContainer">
+        <div className="textInputContainer">
           <Typography variant="h6">Gender</Typography>
-          <Typography>{user.gender}</Typography>
+          <select name="gender" value={values.gender} onChange={handleChange}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
         </div>
         {user.biography && (
-          <div className="textIconContainer">
+          <div className="textInputContainer">
             <Typography variant="h6">Biography</Typography>
-            <Typography>{user.biography}</Typography>
+            <textarea
+              className="textarea"
+              name="biography"
+              value={values.biography}
+              onChange={(e) => {
+                console.log(charsLeft);
+                if (e.target.value.length <= 255) {
+                  setValues({ ...values, biography: e.target.value });
+                  setCharsLeft(e.target.value.length);
+                }
+              }}
+            />
           </div>
         )}
-      </div>
+        <button type="submit">Save</button>
+      </form>
     </Card>
   );
 };
--- src/components/settings/settings.styles.js
+++ src/components/settings/settings.styles.js
@@ -6,12 +6,11 @@ export const styles = makeStyles((theme) => ({
     height: '100%',
     width: '100%',
     boxShadow: '-2px 3px 31px -13px rgba(117,117,117,0.46)',
-    background: "transparent",
-  
+    background: 'transparent',
 
     '& .main': {
       padding: '20px',
-      '& .textIconContainer': {
+      '& .textInputContainer': {
         display: 'flex',
         flexDirection: 'column',
         margin: '20px 0',
@@ -23,11 +22,35 @@ export const styles = makeStyles((theme) => ({
           marginBottom: '5px',
         },
 
-        '& div': {
+        '& input': {
           display: 'flex',
           alignItems: 'center',
-          '& a': {
-            color: 'rgba(0, 0, 0, 0.54)',
+          position: 'relative',
+          justifyContent: 'space-between',
+          border: '2px solid #f0f0f0',
+          borderRadius: '15px',
+          padding: '10px 15px',
+          background: 'white',
+        },
+        '& .textarea': {
+          border: '2px dashed #f0f0f0',
+          borderRadius: '15px',
+          outline: 'none',
+          paddingTop: '10px',
+          paddingBottom: '5%',
+          paddingLeft: '10px',
+          paddingRight: '10px',
+          fontSize: '1.6rem',
+          width: '100%',
+          whiteSpace: 'wrap',
+          resize: 'none',
+          fontFamily: '"Arial", sans-serif',
+          overflow: 'hidden',
+
+          '&::placeholder': {
+            color: '#b7b7b7',
+            textIndent: '.5%',
+            fontFamily: '"Arial", sans-serif',
           },
         },
       },
